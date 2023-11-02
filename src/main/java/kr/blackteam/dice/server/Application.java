5ed9d5d6c80e54954164d2bc7631fa2c4c71c767
Myoung Ha, Ji <webmaster@null0xff.com>
2023-11-02 15:37:35 +0900

    :tada: Spring Boot 3.1.5 프로젝트 생성

---
+++ src/main/java/kr/blackteam/dice/server/Application.java
@@ -0,0 +1,13 @@
+package kr.blackteam.dice.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
