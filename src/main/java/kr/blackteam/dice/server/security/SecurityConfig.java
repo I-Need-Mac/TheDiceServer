a478fd0f2220ca7afc99630b7207ca39c6db8b6a
Myoung Ha, Ji <webmaster@null0xff.com>
2023-11-08 03:34:23 +0900

    :wrench: Security Filter Chain 적용

---
+++ src/main/java/kr/blackteam/dice/server/security/SecurityConfig.java
@@ -0,0 +1,24 @@
+package kr.blackteam.dice.server.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable).httpBasic(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/docs/**").permitAll()
+            .requestMatchers("/api/swagger-ui/**").permitAll().requestMatchers("/api/v1/**")
+            .permitAll().anyRequest().authenticated());
+    return http.build();
+  }
+
+}
