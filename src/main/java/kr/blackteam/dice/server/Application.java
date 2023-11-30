e86e34770352a65733d6a4bb67936462155f4f8d
Myoung Ha, Ji <webmaster@null0xff.com>
2023-11-30 15:30:06 +0900

    :memo: OpenAPI 명세 적용

--- src/main/java/kr/blackteam/dice/server/Application.java
+++ src/main/java/kr/blackteam/dice/server/Application.java
@@ -1,13 +1,26 @@
 package kr.blackteam.dice.server;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@OpenAPIDefinition(
+    servers = {
+        @Server(
+            url = "/",
+            description = "Server URL"
+        )
+    }
+)
+@EnableJpaAuditing
+@EnableJpaRepositories
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
-
 }
