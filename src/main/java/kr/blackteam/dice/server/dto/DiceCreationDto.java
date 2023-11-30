95ea1e122472f10f47cdf1142332035e04aebb47
Myoung Ha, Ji <webmaster@null0xff.com>
2023-11-30 21:53:09 +0900

    :memo: Dice API 명세 추가

--- src/main/java/kr/blackteam/dice/server/dto/DiceCreationDto.java
+++ src/main/java/kr/blackteam/dice/server/dto/DiceCreationDto.java
@@ -1,5 +1,7 @@
 package kr.blackteam.dice.server.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import kr.blackteam.dice.server.model.DiceType;
 import lombok.AllArgsConstructor;
@@ -11,9 +13,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DiceCreationDto {
 
+  @Min(
+      value = 0,
+      message = "Max durability must be zero or positive"
+  )
   private int maxDurability;
+  @Min(
+      value = 0,
+      message = "Current durability must be zero or positive"
+  )
   private int currentDurability;
+  @Min(
+      value = 0,
+      message = "Grade must be zero or positive"
+  )
   private int grade;
+  @NotNull(message = "Dice type must not be null")
   private DiceType type;
+  @NotNull(message = "Player ID must not be null")
   private UUID playerId;
 }
